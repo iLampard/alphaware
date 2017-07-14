c61d5b4f31709349a549a694e8c3004423292c06
iLampard <siqiao_xue@163.com>
2017-07-14 16:56:56 +0800

    clean code

--- alphaware/analyzer/factor_quantile.py
+++ alphaware/analyzer/factor_quantile.py
@@ -13,14 +13,11 @@ class FactorQuantile(FactorEstimator):
         result = pd.DataFrame()
         for date_ in tiaocang_date:
             factor = pd.DataFrame(pd.concat([score[date_], fwd_return[date_]], axis=1))
-            factor = factor.sort_values(by=score.name)
+            factor.sort_values(by=score.name, inplace=True)
             result_quantiles = {}
             for i in range(0, self.quantiles):
-                result_quantiles[score.name + '_' + fwd_return.name + '_' + str(i + 1)] = factor[fwd_return.name][
-                                                                                          i * len(
-                                                                                              factor) / self.quantiles:(
-                                                                                                                           i + 1) * len(
-                                                                                              factor) / self.quantiles].mean()
+                result_quantiles[score.name + '_' + fwd_return.name + '_' + str(i + 1)] \
+                    = factor[fwd_return.name][i * len(factor) / self.quantiles:(i + 1) * len(factor) / self.quantiles].mean()
             result = pd.concat([result, pd.DataFrame(result_quantiles, index=[date_])], axis=0)
         return result
 
@@ -33,7 +30,6 @@ class FactorQuantile(FactorEstimator):
             for alpha in factor_container.alpha_factor_col:
                 alpha_factor = data_df[alpha]
                 calc_data = self._quantile_calc(alpha_factor, fwd_return, tiaocang_date)
-                result = pd.concat(
-                    [result, calc_data], axis=1)
+                result = pd.concat([result, calc_data], axis=1)
 
         return result
